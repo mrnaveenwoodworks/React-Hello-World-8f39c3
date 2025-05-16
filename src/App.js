d16b2ad08ba39cc65211783ca24fd65305db424f
mrnaveenwoodworks <mrnaveenwoodworks@gmail.com>
2025-05-16 19:14:01 +0530

    CODEMATE.ai - upload codebase

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello World</h1>
+        <p>This is my first React application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
